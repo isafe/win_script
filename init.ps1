9030f799bc3139e55b0f0edb19236bcabd5d6d0e
iSec <isafe@users.noreply.github.com>
2023-03-15 01:32:32 +0800

    add remove file

--- init.ps1
+++ init.ps1
@@ -1,5 +1,18 @@
-(New-Object Net.WebClient).DownloadFile("URL","$env:USERPROFILE\Downloads\FILENAME")
-start-process "$env:USERPROFILE\Downloads\FILENAME"
+if([System.IO.File]::Exists("$env:USERPROFILE\Downloads\FILENAME"))
+{
+    Write-Host "File Exists"
+}
+else
+{
+    (New-Object Net.WebClient).DownloadFile(URL,"$env:USERPROFILE\Downloads\FILENAME")
+    start-process "$env:USERPROFILE\Downloads\FILENAME"
+}
+
+if([System.IO.File]::Exists("$env:USERPROFILE\Downloads\FILENAME"))
+{
+    remove-item "$env:USERPROFILE\Downloads\FILENAME" -force
+}
+
 &'C:\Program Files\7-Zip\7z.exe' x "$env:USERPROFILE\Downloads\FILENAME.rar" -o"$env:USERPROFILE\Downloads" -y
 ii $env:USERPROFILE\Downloads
 exit
